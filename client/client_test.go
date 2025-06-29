8c7d18b439e83a04b6d385636c01b62c2ece39ae
Nitesh Sandal <nitesh.sandal@merge.dev>
2025-06-29 10:16:08 -0400

    update test

--- client/client_test.go
+++ client/client_test.go
@@ -4,6 +4,7 @@ package client
 
 import (
 	option "github.com/merge-api/merge-go-client/v2/option"
+	merge "github.com/merge-api/merge-go-client/v2"
 	assert "github.com/stretchr/testify/assert"
 	http "net/http"
 	testing "testing"
@@ -13,7 +14,7 @@ import (
 func TestNewClient(t *testing.T) {
 	t.Run("default", func(t *testing.T) {
 		c := NewClient()
-		assert.Empty(t, c.baseURL)
+		assert.Equal(t, merge.Environments.Production, c.baseURL)
 	})
 
 	t.Run("base url", func(t *testing.T) {
@@ -30,7 +31,7 @@ func TestNewClient(t *testing.T) {
 		c := NewClient(
 			option.WithHTTPClient(httpClient),
 		)
-		assert.Empty(t, c.baseURL)
+		assert.Equal(t, merge.Environments.Production, c.baseURL)
 	})
 
 	t.Run("http header", func(t *testing.T) {
@@ -39,7 +40,7 @@ func TestNewClient(t *testing.T) {
 		c := NewClient(
 			option.WithHTTPHeader(header),
 		)
-		assert.Empty(t, c.baseURL)
+		assert.Equal(t, merge.Environments.Production, c.baseURL)
 		assert.Equal(t, "test", c.header.Get("X-API-Tenancy"))
 	})
 }
